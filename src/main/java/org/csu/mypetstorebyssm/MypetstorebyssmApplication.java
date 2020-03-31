67638937961d3adbd2399e8df3cd7487918f9985
alersaaa <44466602+alersaaa@users.noreply.github.com>
2020-03-31 15:48:28 +0800

    Init MyPetStore Project

---
+++ src/main/java/org/csu/mypetstorebyssm/MypetstorebyssmApplication.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstorebyssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstorebyssmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstorebyssmApplication.class, args);
+    }
+
+}
